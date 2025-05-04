8119388ba4345e90325819d2110ed27acb1385e8
Krzysztof Skarbowski <krzysztof.skarbowski@gmail.com>
2025-05-04 14:51:40 +0200

    Zmiana struktury projektu

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'web_app' title`, () => {
+  it(`should have the 'XSWare' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('web_app');
+    expect(app.title).toEqual('XSWare');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, web_app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Witaj w XSWare Solution');
   });
 });
