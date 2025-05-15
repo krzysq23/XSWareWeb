389c09589ff3832477c44cfa891c07da639650df
Krzysztof Skarbowski <krzysztof.skarbowski@gmail.com>
2025-05-15 15:28:29 +0200

    Dodanie strony logowania i rejestracji

--- src/app/config/app.routes.ts
+++ src/app/config/app.routes.ts
@@ -3,11 +3,15 @@ import { HomeComponent } from '../pages/home/home.component';
 import { DemoComponent } from '../pages/demo/demo.component'; 
 import { ContactComponent } from '../pages/contact/contact.component';
 import { AboutComponent } from '../pages/about/about.component'; 
+import { RegisterComponent } from '../pages/register/register.component'; 
+import { LoginComponent } from '../pages/login/login.component'; 
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'demo', component: DemoComponent },
     { path: 'contact', component: ContactComponent },
-    { path: 'about', component: AboutComponent }
+    { path: 'about', component: AboutComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent }
 ];
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  imports: [CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  focus: any;
+  focus1: any;
+}
