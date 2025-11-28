b49bb3eba49a46b11f10f2f89385288ea34b1fba
Krzysztof Skarbowski <krzysztof.skarbowski@gmail.com>
2025-11-28 11:39:47 +0100

    aktualizacja portfolio i zrealizowanych projektów

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { TabsModule } from "ngx-bootstrap/tabs";
+import { CarouselModule } from "ngx-bootstrap/carousel";
 
 @Component({
   selector: 'app-portfolio',
-  imports: [ TabsModule ],
+  imports: [ 
+    TabsModule,
+    CarouselModule
+  ],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
